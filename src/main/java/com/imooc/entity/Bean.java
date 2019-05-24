29d8cb4cd4457b8df0a5cc8cbbdcc798478293e4
houfei <331488738@qq.com>
2019-05-24 17:49:17 +0800

    集合类型的Bean注入

--- src/main/java/com/imooc/entity/Bean.java
+++ src/main/java/com/imooc/entity/Bean.java
@@ -1,8 +1,89 @@
 package com.imooc.entity;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 public class Bean {
     private String userName;
     private AnotherBean anotherBean;
+
+    private List<String> stringList;
+    private List<AnotherBean> anotherBeanList;
+
+    private Set<String> stringSet;
+    private Set<AnotherBean> anotherBeanSet;
+
+    private Map<String, String> stringMap;
+    private Map<AnotherBean,AnotherBean> anotherBeanMap;
+
+    private Properties properties;
+    private AnotherBean anotherBean2;
+
+    public AnotherBean getAnotherBean2() {
+        return anotherBean2;
+    }
+
+    public void setAnotherBean2(AnotherBean anotherBean2) {
+        this.anotherBean2 = anotherBean2;
+    }
+
+    public List<String> getStringList() {
+        return stringList;
+    }
+
+    public void setStringList(List<String> stringList) {
+        this.stringList = stringList;
+    }
+
+    public List<AnotherBean> getAnotherBeanList() {
+        return anotherBeanList;
+    }
+
+    public void setAnotherBeanList(List<AnotherBean> anotherBeanList) {
+        this.anotherBeanList = anotherBeanList;
+    }
+
+    public Set<String> getStringSet() {
+        return stringSet;
+    }
+
+    public void setStringSet(Set<String> stringSet) {
+        this.stringSet = stringSet;
+    }
+
+    public Set<AnotherBean> getAnotherBeanSet() {
+        return anotherBeanSet;
+    }
+
+    public void setAnotherBeanSet(Set<AnotherBean> anotherBeanSet) {
+        this.anotherBeanSet = anotherBeanSet;
+    }
+
+    public Map<String, String> getStringMap() {
+        return stringMap;
+    }
+
+    public void setStringMap(Map<String, String> stringMap) {
+        this.stringMap = stringMap;
+    }
+
+    public Map<AnotherBean, AnotherBean> getAnotherBeanMap() {
+        return anotherBeanMap;
+    }
+
+    public void setAnotherBeanMap(Map<AnotherBean, AnotherBean> anotherBeanMap) {
+        this.anotherBeanMap = anotherBeanMap;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
 
     public String getUserName() {
         return userName;
@@ -23,5 +104,20 @@ public class Bean {
     public Bean(String userName, AnotherBean anotherBean) {
         this.userName = userName;
         this.anotherBean = anotherBean;
+    }
+
+    @Override
+    public String toString() {
+        return "Bean{" +
+                "userName='" + userName + '\'' +
+                ", anotherBean=" + anotherBean +
+                ", stringList=" + stringList +
+                ", anotherBeanList=" + anotherBeanList +
+                ", stringSet=" + stringSet +
+                ", anotherBeanSet=" + anotherBeanSet +
+                ", stringMap=" + stringMap +
+                ", anotherBeanMap=" + anotherBeanMap +
+                ", properties=" + properties +
+                '}';
     }
 }
--- src/test/java/com/imooc/anotherTest.java
+++ src/test/java/com/imooc/anotherTest.java
@@ -8,10 +8,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class anotherTest {
 
     @Test
-    public void test(){
+    public void test() {
         ApplicationContext context = new ClassPathXmlApplicationContext("anotherapplication.xml");
         Bean bean = (Bean) context.getBean("bean");
         System.out.println("Bean" + bean.getAnotherBean());
         System.out.println("userName: " + bean.getUserName());
+        System.out.println("Bean.getStringList()" + bean.getStringList());
+        System.out.println("Bean.getStringSet()" + bean.getStringSet());
+        System.out.println("Bean.getStringMap()" + bean.getStringMap());
+        System.out.println("Bean.getAnotherBeanList()" + bean.getAnotherBeanList());
+        System.out.println("Bean.getAnotherBeanSet()" + bean.getAnotherBeanSet());
+        System.out.println("Bean.getAnotherBeanMap()" + bean.getAnotherBeanMap());
+        System.out.println("Bean.getProperties()" + bean.getProperties());
+        System.out.println("Bean.getAnotherBean2()" + bean.getAnotherBean2());
     }
 }
